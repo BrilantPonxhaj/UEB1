03b363c8ca1cd94fa2f23bb58d099941b9508202
Brilant <brilant.ponxhaj@student.uni-pr.edu>
2024-01-09 19:38:22 +0100

    mesazh

--- index.js
+++ index.js
@@ -186,6 +186,26 @@ var swiper = new Swiper(".ReviewSlider", {
 });
 
 
+// drag n drop
+function allowDrop(ev) {
+	ev.preventDefault();
+	return false;	
+}
+
+
+function drag(ev) {
+	ev.dataTransfer.setData("text", ev.target.id);
+	return false;
+}
+
+function drop(ev) {
+	ev.preventDefault();
+	var data = ev.dataTransfer.getData("text");
+	var draggedElement = document.getElementById(data);
+	ev.target.appendChild(draggedElement);
+}
+
+
 
 
 //THEME TOGGLE
